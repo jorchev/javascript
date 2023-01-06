b4ce04392fa4b77c285f3f6029cf1a7d05f1244f
Jorge Vega <j.vega@mafasa.org>
2023-01-06 10:36:45 -0700

    03 - Introduccion a la consola

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -2,8 +2,15 @@
 
 let a = 10,
     b = 20,
-    c = 10,
-    d = 10,
+    c = 'Hola ',
+    d = 'Spiderman',
     x = a + b;
 
-console.log( x );
+const saludo = c + d;
+
+// console.log('%c Mis variables','color:yellow; font-weight:bold')
+// console.log({ a });
+// console.log({ b });
+// console.log({ c });
+
+// console.table({ a, b, c, d, x });
