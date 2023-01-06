e6c4afbba50af6f62810c827e6028cb052ba95a0
Jorge Vega <j.vega@mafasa.org>
2023-01-06 11:45:38 -0700

    07 - Prompt, confirm y alert

--- 01-fundamentos/assets/js/alerts.js
+++ 01-fundamentos/assets/js/alerts.js
@@ -1,1 +1,13 @@
 
+// solo en el navegador
+alert('Hola Mundo');
+
+let nombre = prompt('¿Cuál es tu nombre?','Sin nombre');
+console.log(nombre);
+console.log('****' + nombre + '****'); // ''
+
+const seleccion = confirm('¿Estas seguro de borrar esto?');
+console.log(seleccion);
+
+// solo en node
+// console.log( global );
