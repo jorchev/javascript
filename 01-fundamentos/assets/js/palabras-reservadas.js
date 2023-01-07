1582ebf7f48c55335428a882fd9339580c8e795e
Jorge Vega <j.vega@mafasa.org>
2023-01-07 12:26:54 -0700

    09 - Palabras reservadas y nombres de variables

---
+++ 01-fundamentos/assets/js/palabras-reservadas.js
@@ -0,0 +1,30 @@
+
+
+let await = 123;
+
+// const
+
+// if
+
+// try
+
+// while
+
+// for()
+
+// enum
+
+
+
+// How convenient!
+var π = Math.PI;
+
+
+// Ejemplos de nombre de identificadores
+let _objeto$123 = 123;
+let precio99_99 = 123;
+let jugadorConPuntajeMasAlto = 'Jorge';
+
+class JuegoAnio {
+    
+}
