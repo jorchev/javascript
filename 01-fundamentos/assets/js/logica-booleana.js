cb98828e9f29563f275cf258a55c089be1127370
Jorge Vega <j.vega@mafasa.org>
2023-01-12 09:48:35 -0700

    21 - Pro tip: Asignaciones con operadores

--- 01-fundamentos/assets/js/logica-booleana.js
+++ 01-fundamentos/assets/js/logica-booleana.js
@@ -40,3 +40,20 @@ console.log( '4 condiciones', true || true || true || false ) // true
 
 
 console.warn('Asignaciones');
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
+
+const a1 = false && 'Hola mundo' && 150;
+const a2 = 'Hola' && 'Mundo' && soyFalso && true;
+const a3 = soyFalso || 'Ya no soy falso';
+const a4 = soyFalso || soyUndefined || soyNull || 'Ya no soy falso de nuevo' || true;
+const a5 = soyFalso || soyUndefined || regresaTrue() || 'Ya no soy falso de nuevo' || true;
+console.log( a1, a2, a3, a4, a5 );
+
+
+if( regresaFalse() && regresaTrue() && (true || true || true || false) ){
+    console.log('Hacer algo');
+} else {
+    console.log('Hacer otra cosa');
+}
