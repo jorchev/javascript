3488a36d0885bffb4c130748c4ecdb50affc440e
Jorge Vega <jorge.hvr@gmail.com>
2023-01-16 18:13:23 -0700

    30 - Crear barajas de cartas

---
+++ 02-blackjack/assets/js/juego.js
@@ -0,0 +1,33 @@
+/**
+ * 2C = Two of Clubs
+ * 2D = Two of Diamonds
+ * 2H = Two of Hearts
+ * 2S = Two of spades
+ */
+
+let deck = [];
+const tipos = ['C','D','H','S'];
+const especiales = ['A','J','Q','K'];
+
+const crearDeck = () => {
+
+    for( let i = 2; i <= 10; i++ ){
+        for( let tipo of tipos ){
+            deck.push( i + tipo );
+        }
+    }
+
+    for( let especial of especiales ){
+        for( let tipo of tipos ){
+            deck.push( especial + tipo );
+        }
+    }
+
+    console.log(deck);
+    deck = _.shuffle( deck );
+    console.log(deck);
+    return deck;
+
+}
+
+crearDeck();
