a8da4353f379994ceff653f1e4996e5e22f5446e
Jorge Vega <jorge.hvr@gmail.com>
2023-01-18 18:57:33 -0700

    32 - Valor de cada carta

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -47,3 +47,13 @@ const pedirCarta = () => {
 }
 
 // pedirCarta();
+
+const valorCarta = ( carta ) => {
+
+    const valor = carta.substring( 0, carta.length - 1 );
+    return ( isNaN( valor ) ) ? ( valor === 'A' ) ? 11 : 10 : valor * 1;
+
+}
+
+const valor = valorCarta( pedirCarta() );
+console.log( valor );
